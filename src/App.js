3ff9cd9c414ea07ec4d0b0e9e1ac8069fd68f39b
Jose Pagan <jm.pagan@gmail.com>
2019-02-15 19:31:03 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Measure from 'react-measure'
 import './App.css';
 
 class App extends Component {
+  state = {cards:[
+    {text:"omg", id:0},
+    {text:"bbq", id:1},
+    {text:"reactIsCrack",id:2},
+    {text:"is This working?",id:3},
+    {text:"yes It is!",id:4},
+    {text:"node killed the php stars",id:5}
+  ],
+    active:null,
+  gridWidth: null,
+gridColumns: 3,
+gridItemWidth: null}
+  changeActive = (id) =>()=>{
+    this.setState({active:id})
+  }
+  updateWidth(){
+    const {gridWidth, gridColumns} = this.state
+    const gapSize = (gridColumns - 1 ) * 10
+    const newWidth = (gridWidth - gapSize) / gridColumns
+    this.setState({gridItemWidth:newWidth}, console.log('new width', this.state.gridItemWidth))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <Measure bounds onResize={contentRect =>{this.setState({gridWidth:contentRect.bounds.width},this.updateWidth)}}>
+        {({measureRef})=>(
+          <div className="App" ref={measureRef} style={{display:"grid",gridTemplateColumns:"1fr 1fr 1fr", gridGap:10}}>
+            <CardList gridItemWidth={this.state.gridItemWidth} clicked={this.changeActive} active={this.state.active} cards={this.state.cards} className="card"></CardList>
+          </div>
+        ) }
+
+      </Measure>
     );
   }
 }
+  
+
+const CardList = ({cards,active, clicked, gridItemWidth}) => {
+  return  (
+    cards.map(card =>  
+      <div style = {card.id === active?{width: gridItemWidth  * 3 / 2}:{width: gridItemWidth  * 3 / 4}} className={`card ${ card.id === active ? 'active' : 'inactive' }`} 
+           onClick={clicked(card.id)}>{card.text}
+           </div>)
+  )
+}
+
+
+
+
+
+
+
+
+
 
 export default App;
+
